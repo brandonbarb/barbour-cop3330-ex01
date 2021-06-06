ee3b9e625cdaba9a21b833daa4445e12ae55a5ac
Brandon Barbour <brandon8barbour@gmail.com>
2021-06-06 15:11:51 -0400

    typos

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,9 +5,9 @@
 
 import java.util.Scanner;
 
-public class main {
+public class Main {
     public static void main(String[] args) {
-        System.out.println("What is your name?");
+        System.out.print("What is your name? ");
         Scanner input = new Scanner(System.in);
         System.out.println("Hello, " + input.next() + ", nice to meet you!");
     }
